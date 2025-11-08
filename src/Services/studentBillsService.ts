30ad0b6aeb50d39429b20907dd0b2a585f2c2171
Philemon Cobbina (MSC Ghana Ltd.) <philemon.cobbina@msc.com>
2025-11-08 18:00:10 +0000

    Your commit message

--- src/Services/studentBillsService.ts
+++ src/Services/studentBillsService.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 // Base API configuration
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://api.plvcmonline.uk/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
